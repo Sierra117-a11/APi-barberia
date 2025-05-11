9e06b36fd08e775f1e04358b204e9a2fa7384165
josecamargo83 <josecamargo83@gmail.com>
2025-05-10 20:55:16 -0500

    Creación de las carpetas
    Se crean carpetas para separar los archivos y organizar el proyecto

---
+++ BACKEND/main.py
@@ -0,0 +1,36 @@
+# main.py
+from fastapi import FastAPI
+from config.database import engine, Base
+from routes.usuario import router as usuario_router
+from routes.contacto import router as contacto_router
+from routes.agenda import router as agenda_router
+from routes.auth import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI(
+    title="API de Barbería",
+    description="Gestión de usuarios, agenda, contactos y autenticación",
+    version="1.0.0"
+)
+
+@app.on_event("startup")
+def startup():
+    Base.metadata.create_all(bind=engine)
+
+app.include_router(usuario_router, prefix="/api")
+app.include_router(contacto_router, prefix="/api")
+app.include_router(agenda_router, prefix="/api")
+app.include_router(auth_router, prefix="/api")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes poner tu dominio aquí luego
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Bienvenido a la API de Barbería"}
